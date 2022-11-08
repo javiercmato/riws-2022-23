bf3b2ba55d4b43432c11a570b7af786fb5a32cd7
Juan Ramil Díaz <juan.ramil@udc.es>
2022-11-08 19:44:08 +0100

    Esqueleto para frontend, componente principal con plantilla básica, configuración de tailwind y angular y filtro de búsqueda

---
+++ frontend/app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ShopComponent } from './shop/shop.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ShopComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NoopAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
