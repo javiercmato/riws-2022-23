9ea73c6939a6d9e170f1e43b91cf910f4df894d9
Juan Ramil Díaz <juan.ramil@udc.es>
2022-11-11 18:35:52 +0100

    Testing tailwind

--- frontend/app/src/app/app.module.ts
+++ frontend/app/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ShopComponent } from './shop/shop.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,6 +16,13 @@ import { ShopComponent } from './shop/shop.component';
     ShopComponent
   ],
   imports: [
+
+    MatInputModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+
+    ReactiveFormsModule,
+
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule
--- frontend/app/src/app/shop/shop.component.ts
+++ frontend/app/src/app/shop/shop.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
-  styleUrls: ['./shop.component.scss']
+  styleUrls: ['./shop.component.scss'],
 })
 export class ShopComponent implements OnInit {
+  filters: FormGroup = new FormGroup({
+    prices: new FormGroup({
+      priceAll: new FormControl<boolean>(true),
+      price1: new FormControl<boolean>(false),
+      price5: new FormControl<boolean>(false),
+      price10: new FormControl<boolean>(false),
+      priceHigh: new FormControl<boolean>(false),
+    }),
+  });
+  allComplete: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+
+  updateAllComplete() {
+    //this.allComplete = this.task.subtasks != null && this.task.subtasks.every(t => t.completed);
+    this.allComplete =
+      this.filters.get('prices')?.get('price1')?.value &&
+      this.filters.get('prices')?.get('price5')?.value &&
+      this.filters.get('prices')?.get('price10')?.value &&
+      this.filters.get('prices')?.get('priceHigh')?.value
   }
 
+  someComplete(): boolean {
+    return this.filters.get('prices')?.get('price1')?.value ||
+      this.filters.get('prices')?.get('price5')?.value ||
+      this.filters.get('prices')?.get('price10')?.value ||
+      this.filters.get('prices')?.get('priceHigh')?.value &&
+      !this.allComplete;
+  }
+
+  setAll(completed: boolean) {
+    this.allComplete = completed;
+    this.filters.get('prices')?.get('price1')?.patchValue(completed);
+    this.filters.get('prices')?.get('price5')?.patchValue(completed);
+    this.filters.get('prices')?.get('price10')?.patchValue(completed);
+    this.filters.get('prices')?.get('priceHigh')?.patchValue(completed);
+  }
 }
